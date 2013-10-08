bcc545555ad23017a8e364d6ba7a7dc65be3f335
Rainbough Phillips <rainbough@gmail.com>
2013-10-08 09:59:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OddJobs::Application.config.secret_key_base = 'de0fb38f3281ce4c4fa8eb434b7b770d04e119ed676696d8d10959eb28ca495f3bf1678a30ff2cf7676e5938fa78bef1a6327d944ad6c5fb1f62946558d11cd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OddJobs::Application.config.session_store :cookie_store, key: '_oddJobs_session'
